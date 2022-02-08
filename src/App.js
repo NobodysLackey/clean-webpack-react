c1b91592e51999253ed86d88efb94480d1f6b701
Michael Lackey <michael.g.lackey@gmail.com>
2022-02-08 12:32:05 -0600

    set up boilerplate

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import sheet from './styles/App.css'
+
+const App = () => {
+
+  return (
+    <div>
+      <h1>Clean Webpack React</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'build'),
+    open: true,
+    port: 3002
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './public/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(jsx|js)$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', 
+                {
+                  "targets": "defaults" 
+                }
+              ], 
+              '@babel/preset-react'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        type: "asset",
+      },
+    ]
+  }
+}
